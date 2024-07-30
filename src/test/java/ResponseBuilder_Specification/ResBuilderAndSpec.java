3de5904346f85e46cab4d720d0335737cb62de35
sritamkumar123 <sritamkumarrout@gmail.com>
2024-07-30 17:34:17 +0530

    ResSpecBuilder

---
+++ src/test/java/ResponseBuilder_Specification/ResBuilderAndSpec.java
@@ -0,0 +1,54 @@
+package ResponseBuilder_Specification;
+
+
+import org.hamcrest.Matchers;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+
+public class ResBuilderAndSpec {
+	
+	// Using ResSpec and ResSpecBuilder we cab make our code modular
+	
+	ResponseSpecification resSpec;
+	
+	@BeforeClass
+	public void createBuilder() {
+		ResponseSpecBuilder resBuilder = new ResponseSpecBuilder();
+		resBuilder.expectStatusCode(200);
+		resBuilder.expectContentType(ContentType.JSON);
+		resBuilder.expectStatusLine("HTTP/1.1 200 OK");
+		resBuilder.expectResponseTime(Matchers.lessThan(3000L));
+		resSpec = resBuilder.build();
+	}
+
+	@Test
+	public void validateResponse() {
+		
+		RestAssured.given()
+		.baseUri("https://reqres.in/api").basePath("/users")
+		.contentType(ContentType.JSON)
+		.when().get("/2")
+		.then().spec(resSpec);
+		
+	}
+	
+	@Test
+	public void validateResponseWithRegularApproach() {
+		
+		RequestSpecification reqSpec = RestAssured.given();
+		reqSpec.baseUri("https://reqres.in/api").basePath("/users")
+		.contentType(ContentType.JSON);
+		
+		Response res = reqSpec.get("/2");
+		
+		res.then().spec(resSpec);
+		res.prettyPrint();
+	}
+}
